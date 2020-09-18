9560c7fe11da6cc23f7bd0e99a3d90d9e5072a07
louistru <louistru@hotmail.com>
2020-09-18 21:25:39 +0800

    up:

--- indes.tsx
+++ indes.tsx
@@ -2,27 +2,19 @@
 import * as JSX from './jsx';
 
 class Test extends JSX.ViewController<{a: number; b: string}> {
-
 	private _state = this.props;
-
 	test() {
-
 		var test = import('./test');
-
 		console.log(test);
-
 		console.log(this._state);
 	}
 }
 
 class Test2 extends JSX.ViewController<{c: number}> {
-
 	private _c = this.props.c;
-
 	test() {
 		console.log(this.props.c, this._c);
 	}
-
 }
 
 export default function() {
